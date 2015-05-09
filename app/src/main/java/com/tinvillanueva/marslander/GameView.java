23061e10399d226eb0ade0697de95a62407fe84c
tin villanueva <mcpvillanueva@gmail.com>
2015-05-09 21:38:34 +0800

    Fixing terrain & LandingPad

--- app/src/main/java/com/tinvillanueva/marslander/GameView.java
+++ app/src/main/java/com/tinvillanueva/marslander/GameView.java
@@ -34,17 +34,18 @@ public class GameView extends SurfaceView implements Runnable, SurfaceHolder.Cal
 
     //terrain
     private int maxTerrainHeight;
-    private final int MIN_TERRAIN_POINTS = 5;
+    private final int MIN_TERRAIN_POINTS = 6;
 
     private Paint terrainPaint;
     private Path terrainPath;
     private Region terrainRegion;
     private Bitmap terrainTexture;
     private BitmapShader terrainShade;
-    private Random randomTerrain;
+//    private Random randomTerrain;
 
     private Path landingPadPath;
     private Region landingPadRegion;
+    private Region clip;
     private int landingPadWidth;
     private int landingPadX;
     private int landingPadY;
@@ -110,35 +111,32 @@ public class GameView extends SurfaceView implements Runnable, SurfaceHolder.Cal
 
         paint.setColor(Color.GREEN);
         canvas.drawPath(terrainPath, paint);
-
         paint.setColor(Color.RED);
         paint.setStyle(Paint.Style.STROKE);
         canvas.drawPath( landingPadPath, paint);
 
-//        paint.setColor(Color.YELLOW);
-//        canvas.drawPath(terrainPath, paint);
+        paint.setColor(Color.YELLOW);
+        canvas.drawPath(terrainPath, paint);
     }
 
     //setup game
     private void setupGame(){
-        running = false;
         holder = getHolder();
         holder.addCallback(this);
+        running = false;
+        setFocusable(true);
 
         DisplayMetrics metrics = getContext().getResources().getDisplayMetrics();
         screenHeight = metrics.heightPixels;
         screenWidth = metrics.widthPixels;
-        maxTerrainHeight = screenHeight/2;
 
-        setFocusable(true);
+        clip = new Region(0, 0, screenWidth, screenHeight);
 
         paint = new Paint();
-        paint.setColor(Color.WHITE);
+        paint.setColor(Color.BLUE);
         paint.setStrokeWidth(5);
 
-        Path test;
-        test = new Path();
-        test.moveTo(0, screenHeight);
+
         marsTerrain();
     }
 
@@ -150,13 +148,16 @@ public class GameView extends SurfaceView implements Runnable, SurfaceHolder.Cal
         terrainPaint.setColor(0xFFFFFFFF);
         terrainPaint.setStyle(Paint.Style.FILL);
         terrainPaint.setShader(terrainShade);
-        //random terrain path
-        randomTerrain = new Random();
+        //terrain path
         terrainPath = new Path();
         terrainPath.setFillType(Path.FillType.WINDING);
         terrainPath.setLastPoint(0, screenHeight);
+        //random terrain path
+        Random randomTerrain = new Random();
+
         int lastX = 0;
         int lastY = screenHeight - randomTerrain.nextInt(screenHeight/2);
+
         terrainPath.lineTo(lastX, lastY);
 
         int newX = lastX;
@@ -168,39 +169,46 @@ public class GameView extends SurfaceView implements Runnable, SurfaceHolder.Cal
             lastX = newX;
             lastY = newY;
             newX += randomTerrain.nextInt(screenWidth/MIN_TERRAIN_POINTS);
-            newY += screenHeight - randomTerrain.nextInt(maxTerrainHeight);
+            newY += screenHeight - randomTerrain.nextInt(screenHeight/2);
+
             terrainPath.cubicTo(interpolateLinear(lastX, newX, 0.333f), lastY,
                     interpolateLinear(lastX, newX,0.666f), newY, newX, newY);
+
             if (newX > (screenWidth/2) && (!landingPadExists)) {
                 //draw landing area
                 landingPadWidth = (screenWidth/5);
                 landingPadX = newX + randomTerrain.nextInt(screenWidth/MIN_TERRAIN_POINTS);
                 landingPadY = screenHeight - randomTerrain.nextInt(screenWidth/MIN_TERRAIN_POINTS);
-                terrainPath.lineTo(landingPadX, landingPadY);
-                terrainPath.lineTo((landingPadX +landingPadWidth), landingPadY);
+
+                terrainPath.cubicTo(interpolateLinear(newX, landingPadX, 0.333f), newY,
+                        interpolateLinear(newX, landingPadX, 0.666f), landingPadY, landingPadX, landingPadY);
+
+                terrainPath.lineTo((landingPadX + landingPadWidth), landingPadY);
+
                 newX = landingPadX + landingPadWidth;
-                newX = landingPadY;
-                createLandingArea();
+                newY = landingPadY;
+                createLandingPad();
                 landingPadExists = true;
             }
             terrainPath.lineTo(screenWidth, screenHeight);
             terrainPath.close();
+
+            terrainRegion = new Region();
+            terrainRegion.setPath(terrainPath, clip);
         }
-
-
-
     }
 
-    private void createLandingArea() {
-         landingPadPath = new Path();
-         landingPadPath.moveTo(landingPadX, landingPadY);
-         landingPadPath.lineTo(landingPadX + MIN_TERRAIN_POINTS, landingPadY - MIN_TERRAIN_POINTS);
-         landingPadPath.lineTo(landingPadX + landingPadWidth - MIN_TERRAIN_POINTS,
-                landingPadY - MIN_TERRAIN_POINTS);
-         landingPadPath.lineTo(landingPadX + landingPadWidth, landingPadY);
-         landingPadPath.close();
+    private void createLandingPad() {
+        landingPadPath = new Path();
+        landingPadPath.moveTo(landingPadX, landingPadY);
+        landingPadPath.lineTo(landingPadX + 5, landingPadY - 5);
+        landingPadPath.lineTo(landingPadX + landingPadWidth - 5, landingPadY - 5);
+        landingPadPath.lineTo(landingPadX + landingPadWidth, landingPadY);
+        landingPadPath.close();
+        
+        landingPadRegion = new Region();
+        landingPadRegion.setPath(landingPadPath, clip);
     }
-
 
     private int interpolateLinear(int start, int end, float part){
         return (int) (start*(1-part) + end*part);
